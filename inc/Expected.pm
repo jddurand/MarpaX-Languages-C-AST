f9f2e3dcd629d466ce93081d58abd83dbc56cff9
Jean-Damien Durand <jeandamiendurand@free.fr>
2013-05-21 14:18:26 +0200

    test blessed structure result

---
+++ inc/Expected.pm
@@ -0,0 +1,34 @@
+package Expected;
+use strict;
+use warnings FATAL => 'all';
+use Data::Dumper;
+use IO::File;
+use File::Basename;
+use File::Spec;
+
+sub value {
+  my ($class, $got) = @_;
+
+  my ($filename, $directories, $suffix) = fileparse($0, qr/\.[^.]*/);
+  my $datafilename = File::Spec->catfile($directories, "$filename.data");
+  if ($ENV{RELEASE_TESTING} || 0) {
+    #
+    # Untaint $datafilename
+    #
+    ($datafilename) = $datafilename =~ /^(.*)$/;
+    my $fh = IO::File->new($datafilename, 'w');
+    my $d = Data::Dumper->new([$got]);
+    $d->Purity(1)->Terse(1)->Deepcopy(1);
+    print $fh $d->Dump;
+    undef $fh;
+  }
+  my $fh = IO::File->new($datafilename, 'r');
+  $fh->untaint;
+  my $data = do { local $/; <$fh> };
+  undef $fh;
+  my $expected = eval $data;
+
+  return $expected;
+}
+
+1;
--- t/if-if-else.t
+++ t/if-if-else.t
@@ -2,16 +2,20 @@
 use 5.006;
 use strict;
 use warnings FATAL => 'all';
-use Test::More tests => 2;
+use Test::More tests => 3;
 
 BEGIN {
+    push(@INC, 'inc');
     use_ok( 'MarpaX::Languages::C::AST' ) || print "Bail out!\n";
 }
+use Expected;
 
 my $cSourceCode = do { local $/; <DATA> };
 my $cAst = MarpaX::Languages::C::AST->new();
-my $blessed = $cAst->parse(\$cSourceCode);
-ok(defined($blessed));
+my $valuep = $cAst->parse(\$cSourceCode);
+ok(defined($valuep));
+is_deeply($valuep, Expected->value($valuep), 'Expected blessed structure');
+
 __DATA__
 void function() {
 	if (1)
