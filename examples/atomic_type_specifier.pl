a2afa900e825439ad5138748ff1bfdbd25bc6039
Jean-Damien Durand <jeandamiendurand@free.fr>
2013-05-30 00:07:21 +0200

    Move atomic stuff to example since the grammar does not have pb with it (historically c2011 draft had an ambiguity between atomic as type specifier and type qualifier)

---
+++ examples/atomic_type_specifier.pl
@@ -0,0 +1,32 @@
+#!perl
+use strict;
+use warnings FATAL => 'all';
+use MarpaX::Languages::C::AST;
+use Log::Log4perl qw/:easy/;
+use Log::Any::Adapter;
+use Log::Any qw/$log/;
+use Data::Dumper;
+#
+# Init log
+#
+our $defaultLog4perlConf = <<DEFAULT_LOG4PERL_CONF;
+log4perl.rootLogger              = WARN, Screen
+log4perl.appender.Screen         = Log::Log4perl::Appender::Screen
+log4perl.appender.Screen.stderr  = 0
+log4perl.appender.Screen.layout  = PatternLayout
+log4perl.appender.Screen.layout.ConversionPattern = %d %-5p %6P %m{chomp}%n
+DEFAULT_LOG4PERL_CONF
+Log::Log4perl::init(\$defaultLog4perlConf);
+Log::Any::Adapter->set('Log4perl');
+#
+# Parse C
+#
+my $cSourceCode = do { local $/; <DATA> };
+my $cAstObject = MarpaX::Languages::C::AST->new();
+print Dumper($cAstObject->parse(\$cSourceCode));
+__DATA__
+typedef int toto;
+int main(void) {
+  int _Atomic (toto)(void);
+  return toto();
+}
