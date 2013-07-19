be8ffee67f55d764e3c4e39e9570afb6bc6c7107
Jean-Damien Durand <jeandamiendurand@free.fr>
2013-07-19 22:00:07 +0200

    Enable Log4perl and logInfo option

--- examples/file.pl
+++ examples/file.pl
@@ -2,6 +2,22 @@
 use strict;
 use warnings FATAL => 'all';
 use MarpaX::Languages::C::AST;
+use Data::Dumper;
+use Log::Log4perl qw/:easy/;
+use Log::Any::Adapter;
+use Log::Any qw/$log/;
+#
+# Init log
+#
+our $defaultLog4perlConf = <<DEFAULT_LOG4PERL_CONF;
+log4perl.rootLogger              = WARN, Screen
+log4perl.appender.Screen         = Log::Log4perl::Appender::Screen
+log4perl.appender.Screen.stderr  = 0
+log4perl.appender.Screen.layout  = PatternLayout
+log4perl.appender.Screen.layout.ConversionPattern = %d %-5p %6P %m{chomp}%n
+DEFAULT_LOG4PERL_CONF
+Log::Log4perl::init(\$defaultLog4perlConf);
+Log::Any::Adapter->set('Log4perl');
 
 #
 # Parse C
@@ -11,5 +27,5 @@ open(FILE, '<', $file) || die "Cannot open $file, $!\n";
 my $cSourceCode = do { local $/; <FILE> };
 close(FILE) || warn "Cannot close $file, $!\n";
 
-my $cAstObject = MarpaX::Languages::C::AST->new();
+my $cAstObject = MarpaX::Languages::C::AST->new(logInfo => 1);
 print Dumper($cAstObject->parse(\$cSourceCode));
