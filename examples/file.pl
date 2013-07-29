4ac28cb857aef349089ce340588f6795b636ef04
Jean-Damien Durand <jeandamiendurand@free.fr>
2013-07-29 06:02:33 +0200

    Fix logInfo parameter

--- examples/file.pl
+++ examples/file.pl
@@ -27,4 +27,4 @@ open(FILE, '<', $file) || die "Cannot open $file, $!\n";
 my $cSourceCode = do { local $/; <FILE> };
 close(FILE) || warn "Cannot close $file, $!\n";
 
-MarpaX::Languages::C::AST->new(logInfo => 1)->parse(\$cSourceCode)->value();
+MarpaX::Languages::C::AST->new(logInfo => ['IDENTIFIER'])->parse(\$cSourceCode)->value();
