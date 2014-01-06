3295949f17c97a1a88a0138e5d3429a0700354dc
Jean-Damien Durand <jeandamiendurand@free.fr>
2014-01-06 13:46:29 +0100

    scan.t: t+

--- inc/scan.c
+++ inc/scan.c
@@ -1,10 +1,18 @@
+/*
+  defines_no_args test
+*/
 #define MACRO_NO_ARGS_01
 #define MACRO_NO_ARGS_02 something
 
+/*
+  defines_args test
+*/
 #define MACRO_NO_ARGS_01(a)
 #define MACRO_NO_ARGS_02 (b,    c) something(b) + else(c) \
 continued
-
+/*
+  parsed_fdecls test
+*/
 int func1(int x1, double *x2, float *( f1)(int x11, double x12));
 int func2(int x1, double *x2, float *(*f1)(int x11, double x12));
 int func3(int   , double *  , float *(*  )(int    , double    ));
--- t/scan.t
+++ t/scan.t
@@ -1,4 +1,4 @@
-#!perl
+#!perl -T
 use strict;
 use warnings FATAL => 'all';
 use Test::More tests => 5;
